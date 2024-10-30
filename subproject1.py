047175e4b72a81c42d098c99f793a40d848e85b4
ingriun <ingridundeberget@gmail.com>
2024-10-30 10:08:11 +0100

    Update subproject1.py

--- subproject1.py
+++ subproject1.py
@@ -1,12 +1,23 @@
 import numpy as np
 
+#######initializing variables######
 h_bar = 1.05 * 10**(-31)
 m = float
 dim = int
-N = int
-Psi = np.zeros(N, N)
+Psi = np.zeros(dim, dim)
+V = 'formula'
+a = 'lattice spacing'
+###########
 
 
-def hamiltonian():
-    np.zeros()
-    
+def hamiltonian(Psi, dim, V, x):
+    # Initialise the hamiltonian as 2dim-array  
+    H = []
+
+    # calculate the lattice discretized 2nd derivative of Psi
+    Psi2nd_array = []
+
+    for n in (0..dim):
+        Psi2nd_array[n] = (Psi[n][x+a] - 2**Psi[n] + Psi[n][x-a])/a
+
+    Psi2nd_float = np.sum(Psi2nd_array)
