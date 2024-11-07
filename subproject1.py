0ec28778a06f19d32dd8b08a9fcfae22ad297084
Max Simonnet <max.simonnet07@gmail.com>
2024-11-07 16:27:51 +0100

    created kineticEnergy function

--- subproject1.py
+++ subproject1.py
@@ -39,10 +39,14 @@ y = derivative(psi)
 print("2nd-derivative :")
 print(y)
 
+def kineticEnergy(psi):
+    k_hat = np.ones(psi.shape)
+    k_hat = -1/(2*mu*epsilon**2)*k_hat
+
 
 def potential(psi):
     # Initialize V_psi to have the same shape as psi
-    v = np.zeros(psi.shape) 
+    v_hat = np.zeros(psi.shape) 
 
     # Number of point along an axis
     N = list(psi.shape)[0]
@@ -51,7 +55,7 @@ def potential(psi):
     indices = np.arange(N)
 
     # Calculate the potential
-    v = mu/8*((epsilon**2 * indices**2 - 1)**2)
+    v_hat = mu/8*((epsilon**2 * indices**2 - 1)**2)
 
     return v
 
@@ -64,9 +68,11 @@ def hamiltonian(psi):
 
     psi_2nd = derivative(psi)    
 
-    v = potential(psi)
+    v_hat = potential(psi)
+
+    k_hat = kineticEnergy(psi)
     # Calculate the hamiltonian
-    h_hat = -1/(2*mu*epsilon**2)*psi_2nd + v * psi
+    h_hat = k_hat*psi_2nd + v_hat * psi
     
     return h_hat
 
@@ -81,29 +87,21 @@ def euler_integrator(psi): # add tau_hat in parameters?
 def second_order_integrator(psi):   # add tau_hat in parameters?
     return psi - 1j * tau_hat * hamiltonian(psi) - (tau_hat**2 / 2) * (hamiltonian(hamiltonian(psi)))
 
-# strang splitting integrator
-def strang_splitting_integrator(Psi, H_hat):
+def strang_splitting_integrator(psi):
     # Split Hamiltonian into kinetic and potential parts
     v_half = np.exp(-1j * (tau_hat / 2) * potential(psi))  # e^(-i*tau_hat/2 * V)
     
-    eta = v_half * Psi #apply V_half to psi in position space
+    # Apply potential
+    eta = v_half * psi
 
-    #fourier transform to momentum space for kinetic term
+    #fourier transform to momentum space
     eta_tilde = np.fft.fftn(eta)
-    # Define the Fourier space wave numbers
-    k = np.fft.fftfreq(N, d=epsilon) * 2 * np.pi  # FFT frequencies, scaled
-    k_mesh = np.meshgrid(*([k] * dim), indexing='ij')  # Create a meshgrid for each dimension
 
-    # Calculate eigenvalues of K_hat in Fourier space using the known formula
-    K_eigenvalues = sum((2 / (mu * epsilon**2)) * np.sin(k_dim / 2)**2 for k_dim in k_mesh)
-
-    # Define the kinetic evolution operator in Fourier space
-    def kinetic_evolution_operator(tau_hat):
-        return np.exp(-1j * tau_hat * K_eigenvalues)
-
-    K_exp = kinetic_evolution_operator(tau_hat)
+    v_eta = potential(eta) #calculate potential
+    k_hat = hamiltonian(eta) - v_eta #calculate kinetic hamiltonian by removing the V part (setting V=0)
+    k_exp = np.exp(-1j * (tau_hat) * k_hat) #kinetic evolution oeprator
 
     #apply kinetic part
-    xi = np.fft.ifftn(K_exp * eta_tilde) #transform back to position space
+    xi = np.fft.ifft2(k_exp * eta_tilde) #transform back to position space
 
     return v_half * xi
