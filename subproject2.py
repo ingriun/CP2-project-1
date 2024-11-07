6c5f2b5409845cb0a0ff6754381d2d7b1a3e521e
ingriun <ingridundeberget@gmail.com>
2024-11-07 16:20:11 +0100

    Update subproject2.py

--- subproject2.py
+++ subproject2.py
@@ -15,8 +15,8 @@ def linearityTest(psi1, psi2):
     - True if linear, False if not
 
     """
-    a = random.randint(0, 50)
-    b = random.randint(0, 50)
+    a = random.uniform(0, 50) + 1j * random.uniform(0, 50)
+    b = random.uniform(0, 50) + 1j * random.uniform(0, 50)
 
     psiLeft = a * psi1 + b * psi2
 
@@ -26,31 +26,6 @@ def linearityTest(psi1, psi2):
 
     return np.allclose(leftSide, rightSide)
 
-
-
-"""
-def test_linearity(H, psi1, psi2, a, b):
-
-Test the linearity of the hamiltonian operator.
-
-Parameters:
-- H: hamiltonian operator.
-- psi1, psi2: Two sample wavefunctions.
-- a, b: Scalars for the linearity test.
-
-Returns:
-- True if linearity holds, False otherwise.
-
-# Compute H(a * psi1 + b * psi2)
-left_side = H @ (a * psi1 + b * psi2)
-
-# Compute a * H(psi1) + b * H(psi2)
-right_side = a * (H @ psi1) + b * (H @ psi2)
-
-# Check if the two sides are approximately equal
-return np.allclose(left_side, right_side)
-
-"""
 
 def hermiticityTest(psi1, psi2):
     """
@@ -74,27 +49,7 @@ def hermiticityTest(psi1, psi2):
     rightSide = np.sum((h_psi1, psi2))
 
     return np.allclose(leftSide, rightSide)
-"""
 
-def test_hermitian(H, psi1, psi2):
-
-Test the Hermitian property of the hamiltonian operator.
-
-Parameters:
-- H: hamiltonian operator.
-- psi1, psi2: Two sample wavefunctions.
-
-Returns:
-- True if the Hermitian property holds, False otherwise.
-
-# Inner products
-left_side = np.vdot(psi1, H @ psi2)  # <psi1 | H psi2>
-right_side = np.vdot(H @ psi1, psi2)  # <H psi1 | psi2>
-
-# Check if left_side equals the complex conjugate of right_side
-return np.allclose(left_side, right_side)
-
-"""
 
 def positivityTest(psi):
     """
