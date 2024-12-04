simplified integrator test function & corrected conjugate gradient

subproject2.py
@@ -333,52 +333,66 @@ def testUnitarity():
 def testEigenvalue():
     print("---eigenvalueTest--- \n")
 
-    print("For different number of dimension :")
+    print("For different number of dimension :\n")
     for d in range(1,5):
         print("dim = ", d, " :")
         i=0
         while i < 10:
             print(eigenvalueTest(d, 5))
             i = i+1
+        print("\n")
     
     print("\n")
 
-    print("For different value of N")
+    print("For different value of N : \n")
     for n in [15, 55, 91]:
         print("N = ",n," : ") 
         i = 0 
         while i < 10:
             print(eigenvalueTest(2, n))
             i = i+1
-
+        print("\n")
 
 # On Second order & Strang-Splitting integrators
+
+def integDiv(psi,tau_hat):
+    for i in range(4):
+        print("tau_hat = ", tau_hat, " :")
+        print("---> Divergence : ")
+
+        rightSide = psi
+        leftSide = psi
+
+        for m in np.arange(0,1,tau_hat):
+            rightSide = second_order_integrator(rightSide, tau_hat)
+            leftSide = strang_splitting_integrator(leftSide, tau_hat)
+                          
+        divergence = np.abs(leftSide - rightSide)
+        div_max = np.max(divergence)        
+
+        print("-----------> ", div_max)
+        tau_hat = tau_hat/10
+    print("\n")
+
 
 def testIntegrators():
     print("---integratorsTest--- \n")    
 
-    print("For different number of dimension :")
+    print("For different number of dimension : \n")
     for d in range(1,5):
         print("dim = ",d," : ")
 
-        tau_hat = 0.1
-        for n in range(4):
-            tau_hat = tau_hat/10
-            print("tau_hat = ", tau_hat, " :")
-            print("---> Divergence : ")
-            psi = ndim_Random(d, 5)
-            rightSide = psi
-            leftSide = psi
+        psi = ndim_Random(d, 5)
+        integDiv(psi, tau_hat=1)
 
-            for m in np.arange(0,1,tau_hat):
-                rightSide = second_order_integrator(rightSide, tau_hat)
-                leftSide = strang_splitting_integrator(leftSide, tau_hat)
-                          
-            divergence = np.abs(leftSide - rightSide)
-            div_max = np.linalg.norm(divergence)        
 
-            print("-----------> ", div_max)
-    tau_hat = 1
+    print("For different value of N \n")
+    for n in [15, 55, 91]:
+        print("N = ",n," : ") 
+
+        psi = ndim_Random(1, n)
+        integDiv(psi, tau_hat=1)
+
 
 
 

subproject7.py
@@ -1,5 +1,30 @@
 import numpy as np
-from subproject1 import hamiltonian, ndim_Random, dim, N
+from subproject1 import hamiltonian, ndim_Random, dim, N, ndim_Ones
+
+
+def conjugateGradient(mat, tol, max_iter):
+    b = 1
+    x = 0*ndim_Ones(mat.ndim, mat.shape[0])
+    r_new = ndim_Ones(mat.ndim, mat.shape[0]) * b - np.dot(mat,x)
+    r_old = r_new
+    p = r_new
+
+    k=0
+    while k < max_iter:
+        mat_p = np.dot(mat,p)
+        alpha = (np.dot(np.transpose(r_new),r_new))/(np.dot(np.transpose(p),mat_p))
+        x = x + np.dot(alpha,p)
+        r_new = r_new - np.dot(alpha,mat_p)
+
+        if r_new < tol:
+            mat_inv = x
+            break
+
+        beta = (np.dot(np.transpose(r_new),r_new))/(np.dot(np.transpose(r_old),r_old))
+        p = r_new + beta*p
+        r_old = r_new
+        k = k+1
+    return mat_inv
 
 def power_method(Q, tol=1e-6, max_iter=10000):
     """define the power method to compute the largest eigenvalue and corresponding eigenvector of an operator Q