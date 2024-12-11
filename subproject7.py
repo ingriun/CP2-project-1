f02b06aedf1a5f45d42ec0d5246cbcb0d007ba0d
ingriun <ingridundeberget@gmail.com>
2024-12-11 09:46:53 +0100

    gram schmidt

--- subproject7.py
+++ subproject7.py
@@ -5,36 +5,22 @@ from subproject1 import hamiltonian, ndim_Random, dim, N, ndim_Ones
 def conjugateGradient(mat, tol, max_iter):
     b = 1
     x = 0*ndim_Ones(mat.ndim, mat.shape[0])
-<<<<<<< HEAD
-    r_new = ndim_Ones(mat.ndim, mat.shape[0]) * b - mat*x
-=======
     r_new = ndim_Ones(mat.ndim, mat.shape[0]) * b - np.dot(mat,x)
->>>>>>> 991db796024f344ee972bbb729e6c7c7b79328cf
     r_old = r_new
     p = r_new
 
     k=0
     while k < max_iter:
-<<<<<<< HEAD
-        alpha = (np.vdot(np.transpose(r_new),r_new))/(np.vdot(np.transpose(p),mat*p))
-        x = x + alpha*p
-        r_new = r_new - alpha*p
-=======
         mat_p = np.dot(mat,p)
         alpha = (np.dot(np.transpose(r_new),r_new))/(np.dot(np.transpose(p),mat_p))
         x = x + np.dot(alpha,p)
         r_new = r_new - np.dot(alpha,mat_p)
->>>>>>> 991db796024f344ee972bbb729e6c7c7b79328cf
 
         if r_new < tol:
             mat_inv = x
             break
 
-<<<<<<< HEAD
-        beta = (np.vdot(np.transpose(r_new),r_new))/(np.vdot(np.transpose(r_old),r_old))
-=======
         beta = (np.dot(np.transpose(r_new),r_new))/(np.dot(np.transpose(r_old),r_old))
->>>>>>> 991db796024f344ee972bbb729e6c7c7b79328cf
         p = r_new + beta*p
         r_old = r_new
         k = k+1
@@ -89,6 +75,29 @@ def power_method(Q, tol=1e-6, max_iter=10000):
 #defire Hamiltonian as the function for the operator
 #def apply_hamiltonian(psi):
     #return hamiltonian(psi)
+
+def gram_schmidt(V):
+    """Define the gram-schmidt process to orthonormalise the eigenvectors
+
+    Parameters:
+    V: ndarray
+        Consisting of column vectors that needs to be orthonormalised.
+
+    Returns:
+    U: ndarray
+        Orthonormalised column vectors    
+    """
+    n, k = V.shape()
+    U = np.zeros(n, k)
+    U[:, 0] = V[:, 0]/np.linalg.norm(V[:, 0])
+
+    for i in range(1, k):
+        U[:, i] = V[:, i]
+        for j in range(i):
+            U[:, i] = U[:, i] - np.dot(U[:, j], U[:, i])*U[:, j]
+        U[:, i] = V[:, i]/np.linalg.norm(V[:, i])
+
+    return U
 
 #run the power method
 largest_eigenvalue, eigenvector = power_method(hamiltonian)
