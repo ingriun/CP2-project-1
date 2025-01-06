42309367c69153eaac93dbed89c0a22063984fd2
chloelcon <me@chloelee.de>
2025-01-06 18:01:31 +0100

    Update subproject5.py

--- subproject5.py
+++ subproject5.py
@@ -1,21 +1,19 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
-import random
-from subproject1 import strang_splitting_integrator, ndim_Ones, potential
-from subproject1 import N, mu, epsilon, tau_hat, dim
+from subproject1 import strang_splitting_integrator, ndim_Ones, potential, N, tau_hat
 
 def initialWavepacket(dim, N):
     x_positions = np.arange(N**dim)
     psi = ndim_Ones(dim, N)
 
-    psi =  np.exp(-(x_positions-65)**2/(2*5**2))
+    psi =  np.exp(-(x_positions-40)**2/(2*2**2))
 
 
     plt.plot(x_positions, psi.flatten())
     plt.title("Initial Wave Packet")
     plt.xlabel("Position")
-    plt.xlim(0,200)
+    plt.xlim(0,100)
     plt.ylabel("|ψ|")
     plt.show()
     return x_positions, psi
@@ -25,7 +23,7 @@ def initialWavepacket(dim, N):
 def animate_wave_function(dim, N, num_frames=100, integrator=strang_splitting_integrator):
     # Initialize psi
     x_positions, psi = initialWavepacket(dim, N)
-    #psi = ndim_Random(dim, N)
+    
 
     fig, ax = plt.subplots(figsize=(8, 6))
     
@@ -36,7 +34,7 @@ def animate_wave_function(dim, N, num_frames=100, integrator=strang_splitting_in
     line, = ax.plot(x_positions, (psi.flatten()), 'lightgrey', label="Initial Ψ")
     ax.plot(x_positions, potential(psi.shape))
     ax.set_ylim(0, 1.5 * np.max(np.abs(psi)))
-    ax.set_xlim(0,200)
+    ax.set_xlim(0,100)
     ax.set_title("Time Evolution of the Wave Function")
     ax.set_xlabel("Position")
     ax.set_ylabel("Ψ")
@@ -57,6 +55,17 @@ def animate_wave_function(dim, N, num_frames=100, integrator=strang_splitting_in
         # Update the plot 
         line.set_ydata(np.abs(psi_flat))  # Plot the magnitude of psi
         line.set_color('g')
+
+        """if frame == 500:
+            plt.figure()
+            plt.plot(x_positions, np.abs(psi_flat), label="Ψ at specific time")
+            plt.title("Wave Function for N=100")
+            plt.xlabel("Position")
+            plt.ylabel("|Ψ|")
+            plt.xlim(0,100)
+            plt.legend()
+            plt.show()"""
+
         return line,
 
     # Create the animation
